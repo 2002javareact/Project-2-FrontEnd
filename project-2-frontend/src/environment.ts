385489f65ba0ad15fc92dc0c4e81b8fa261e021a
EngineerMel <ramelhaines25@gmail.com>
2020-03-27 18:53:47 -0400

    redux signup and login complete, still need to style

--- project-2-frontend/src/environment.ts
+++ project-2-frontend/src/environment.ts
@@ -1,14 +1,13 @@
-
 const devEnvironment = {
-    projectBaseUrl:'http://localhost:2525'
-}
+  projectBaseUrl: "http://localhost:2525"
+};
 
 const prodEnvironment = {
-    projectBaseUrl:'http://'
-}
+  projectBaseUrl: "http://dbwildcards.cjxkscd0i7zp.us-east-1.rds.amazonaws.com"
+};
 
-export let environment = prodEnvironment
+export let environment = devEnvironment;
 
-if(process.env.REACT_APP_ENV === 'production'){
-    environment = prodEnvironment
+if (process.env.REACT_APP_ENV === "production") {
+  environment = prodEnvironment;
 }
